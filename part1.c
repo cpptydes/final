c39f3b612ee2d73dba4f88c29507ae34528e9bde
Tyler Kloss <tkloss@cpp.edu>
2021-12-04 09:22:33 +0000

    completes part1 shell source code and creates part2 file

--- part1.c
+++ part1.c
@@ -5,12 +5,127 @@
 #include <stdio.h>
 #include <string.h>
 
-//Function Declarations for builtin shell commands:
+// Function Declarations for builtin shell commands:
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
 
 // Listof builtin commands, followed by their corresponding functions.
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
 
-//Builtin function implementations.
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
 
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+// Builtin function implementations.
+/**
+   @brief Builtin command: change directory.
+   @param args List of args. args[0] is "cd". args{1} is in the directory.
+   @return Always returns 1, to continue executing.
+ */
+int lsh_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+/**
+   @brief Builtin command: print help.
+   @param args List of args. Not examined.
+   @return Always returns 1, to continute executing.
+ */
+int lsh_help(char **args)
+{
+  int i;
+  printf("Tyler Kloss' LSH (following Stephen Brennan's guide)\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The follwing are built in:\n");
+
+  for (i=0; i<lsh_num_builtins(); i++) {
+    printf( "%s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+/**
+   @brief Builtin command: exit.
+   @param args List of args. Not examined.
+   @return Always returns 0, to terminate execution.
+ */
+int lsh_exit(char **args)
+{
+  return 0;
+}
+
+/**
+   @brief Launch a program and wait for it to terminate.
+   @param args Null terminated list of arguments (including program).
+   @return Always returns 1, to continute execution.
+ */
+int lsh_launch(char **args)
+{
+  pid_t pid;
+  int status;
+
+  pid = fork();
+  if (pid==0) {
+    // child process
+    if (execvp(args[0], args)==-1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid<0) {
+    //Error forking
+    perror("lsh");
+  } else {
+    // parent process
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
+}
+
+/**
+   @brief Execute shell built-in or launch program. 
+   @param args Null terminated list of arguments.
+   @return 1 if the shell should continue running, 0 if it should terminate
+ */
+int lsh_execute(char **args)
+{
+  int i;
+
+  if(args[0]==NULL) {
+    // an empty command was entered.
+
+    return 1;
+  }
+
+  for (i=0; i<lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i])==0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+  return lsh_launch(args);
+}
 
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
